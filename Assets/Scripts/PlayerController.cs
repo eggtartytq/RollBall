4c168b1798c09919556949b6bd9f3c76144a86e9
eggtartytq <50469264+eggtartytq@users.noreply.github.com>
2020-09-15 13:59:58 -1200

    update to teacher's file

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,66 +4,87 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 
-
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public int totalPickups = 8;
     public TextMeshProUGUI countText;
-    public GameObject winTextObject;
-
-    
+    public TextMeshProUGUI winTextObject;
 
     private Rigidbody rb;
     private int count;
     private float movementX;
     private float movementY;
+    private bool gameOver;
+    private Timer timer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+
         count = 0;
         SetCountText();
-        winTextObject.SetActive(false);
+
+        //winTextObject.SetActive(false);
+        winTextObject.SetText("");
+
+        gameOver = false;
+
+        // Get reference to timer
+        timer = GameObject.FindObjectOfType<Timer>();
     }
 
-    private void OnMove(InputValue movementValue)
+    void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
     void SetCountText()
     {
-        countText.text = "count: " + count.ToString();
-       
-            if (count >= 13)
-            {
-                GameObject.Find("Player").SendMessage("Finish");
-                winTextObject.SetActive(true);
-            }
-       
+        countText.text = "Count: " + count;
+
+        if (count == totalPickups)
+        {
+            winTextObject.SetText("You win!!");
+            gameOver = true;
+
+            // Stop the timer
+            timer.enabled = false;
+        }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
-
-    }   
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log(other.gameObject.tag);
+        if (gameOver)
+            return;
+
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
-            count = count + 1;
+            count++;
             SetCountText();
         }
     }
 
-   
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NoMoreTime()
+    {
+        winTextObject.SetText("You Lose!");
+        gameOver = true;
+    }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿//using SceneManager to change scene
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+  public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
